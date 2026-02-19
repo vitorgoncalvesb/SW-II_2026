61d554b6c38a982b3bfc47586c3be79d44952042
Vitor G. <160020798+vitorgoncalvesb@users.noreply.github.com>
2026-02-19 11:56:55 -0300

    Add files via upload

---
+++ aula_01/Exemplo_01.php
@@ -0,0 +1,5 @@
+<?php
+$nome = "João Pedro";
+
+echo "Olá, $nome! Seja bem-vindo à página.";
+?>
---
+++ aula_01/Exemplo_02.php
@@ -0,0 +1,10 @@
+<?php
+$nota1 = 8;
+$nota2 = 7.5;
+$nota3 = 9;
+
+$media = ($nota1 + $nota2 + $nota3) / 3;
+
+echo "As notas são: $nota1, $nota2, $nota3.<br>";
+echo "A média é: " . number_format($media, 2);
+?>
